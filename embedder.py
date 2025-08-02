b7074cb45bd1d26884f62b5121ddf12339356a29
ISHANT-GUPTA <ishantg0505@gmail.com>
2025-08-02 22:41:42 +0530

    Updated embedder

--- embedder.py
+++ embedder.py
@@ -1,22 +1,38 @@
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import MarkdownTextSplitter, RecursiveCharacterTextSplitter
+from llama_cloud_services import LlamaParse
+from langchain.schema import Document
 import os
 from dotenv import load_dotenv
 
+# Load Environment variables
 load_dotenv()
 
+# Define the parser
+parser = LlamaParse() # type: ignore
+
+# Set vector database directory
 current_dir = os.getcwd()
 persistent_dir = os.path.join(current_dir, "db", "doc_embeddings")
 
-file_path = "doc.pdf"
 
-loader = PyPDFLoader(file_path=file_path)
-documents = loader.load()
-print(len(documents))
+# Parse the pdf file
+file_path = "./test_docs/1_rag_doc.pdf"  # Sample file
+result = parser.parse(file_path=file_path)
 
-splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=200)
+documents = []
+f = open('parsed_md.txt', 'w')
+for page in result.pages: # type: ignore
+    if not page.md is None:
+        documents.append(Document(page_content=page.md)) # type: ignore
+        f.write(page.md + "\n\n")
+f.close()
+
+
+# splitter = MarkdownTextSplitter(chunk_size = 2000, chunk_overlap=300)
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
 docs = splitter.split_documents(documents=documents)
 
 print(f"Number of document chunks: {len(docs)}")
